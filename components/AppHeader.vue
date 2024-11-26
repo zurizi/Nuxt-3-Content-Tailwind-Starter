6f1fd41dc4dd9b9d1d64ff330cd4dbc743e19793
zurizi <muhzulham25@gmail.com>
2024-11-27 05:17:49 +0800

    [update] header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,18 +2,18 @@
   <header class="text-white bg-gray-800">
     <div class="container flex items-center justify-between px-6 py-4 mx-auto">
       <div class="text-2xl font-bold">
-        <nuxt-link to="/">MyLogo</nuxt-link>
+        <nuxt-link to="/">Tailwind Nuxt 3 Starter</nuxt-link>
       </div>
       <nav>
         <ul class="flex space-x-4">
           <li>
-            <nuxt-link to="/" class="hover:text-gray-400">Home</nuxt-link>
+            <nuxt-link to="/" class="hover:text-gray-400">link</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/" class="hover:text-gray-400">About</nuxt-link>
+            <nuxt-link to="/" class="hover:text-gray-400">link 2</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/" class="hover:text-gray-400">Contact</nuxt-link>
+            <nuxt-link to="/" class="hover:text-gray-400">link 3</nuxt-link>
           </li>
         </ul>
       </nav>
@@ -22,9 +22,7 @@
 </template>
 
 <script lang="ts" setup>
-// Anda dapat menambahkan script TypeScript di sini jika diperlukan
 </script>
 
 <style scoped>
-/* Tambahkan style tambahan jika diperlukan */
 </style>
